c51f0aafc9e6038129e42110c9dc8cca9858e325
Vinicius R. Lemos <vinycius1993@gmail.com>
2022-10-15 10:26:32 -0300

    comentando o código

--- C/vetor-soma-media/vetor-soma-media.cpp
+++ C/vetor-soma-media/vetor-soma-media.cpp
@@ -1,25 +1,24 @@
 #include<stdio.h>
 #define TAM 10
-/* 1. Escrever um programa que leia um vetor de 10 posições de inteiros e
+/* 1. Escreva um programa que leia um vetor de 10 posições de inteiros e
 apresente o somatório e a média aritmética de todos os valores. */
 
+//Função que retorna um inteiro. A função recebe como parâmetro um ponteiro do vetor.
 int somarVetor (int *vetor) {
 	int soma = 0;
 	for (int i = 0; i < 10; i++) {
 		soma = soma + vetor[i];
-	}
-	
+	}	
 	return soma;
 }
-
+//Função que retorna um float. A função recebe como parâmetro um ponteiro do vetor.
 float calcularMediaVetor (int *vetor) {
 	float media = 0;
 	int tot = 0;
-	
+	//Laço de repetição para varrer o vetor, inserindo o valor de cada posição na variável'media'.
 	for (int i = 0; i < TAM; i++) {
 		media = media + vetor[i];
 	}
-	
 	return media / TAM;
 }
 
@@ -35,8 +34,7 @@ int main (void) {
 	media = calcularMediaVetor(vetor);
 	
 	printf ("A soma dos elementos do vetor eh: %i \n", soma);
-	printf ("A media dos elementos do vetor eh: %.1f", media);
-	
+	printf ("A media dos elementos do vetor eh: %.1f", media);	
 	
 return 0;
 }
